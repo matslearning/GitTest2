a26559e71d24316687cd6d4d8fc6866cd5aedc4c
Mats <matslearning@outlook.com>
2020-08-26 08:03:55 +0200

    Liten ändring

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing Git!!!");
         }
     }
 }
